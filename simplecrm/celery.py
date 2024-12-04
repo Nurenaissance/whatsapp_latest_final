c38d8af7b6383a2746521a579849f2c3c8746cd3
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-12-04 19:51:34 +0530

    04/12/24

--- simplecrm/celery.py
+++ simplecrm/celery.py
@@ -1,9 +1,14 @@
 import os
-from simplecrm.celery import Celery
+from celery import Celery
 
-# Set the default Django settings module
+# Set default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplecrm.settings')
 
+# Create Celery app
 app = Celery('simplecrm')
+
+# Load configuration from Django settings
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Autodiscover tasks from all registered Django apps
 app.autodiscover_tasks()
